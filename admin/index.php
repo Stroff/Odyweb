dc736e21b378364b02168d85c62eddfcca4b7f77
niliah <niliah@odysse-serveur.com>
2010-11-06 19:42:04 +0800

    Correction warning 'session_cache_limiter': Include de fonctions.php present deux fois
    Signed-off-by: niliah <niliah@odyss�e-serveur.com>

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,7 @@
 ﻿<?php
 $secure_lvl=2;
 include '../secure.php';
-include_once '../include/php/fonctions.php';
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
