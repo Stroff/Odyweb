4597ecd0320789127aac94209e5bd3aca5cd93fa
Damien <damien@macuser.fr>
2010-12-06 03:12:08 +0100

    Changement message de maintenance

--- maintenance.php
+++ maintenance.php
@@ -20,7 +20,12 @@ include "include/template/header_cadres.php"; ?>
 
 <h2>Maintenance</h2>
 <p style="padding-left:10px;padding-top:20px;">Le serveur est actuellement en maintenance. Veuillez revenir plus tard.</p>
-<br/> <br/> <br/> 						</div>
+<br/> <br/> <br/> 						
+<p>Comme vous avez peut-être pu le constater par vous-même, le serveur a arrêté de fonctionner à 20h, n’ayant pu redémarrer correctement suite à un important crash de la base de données.</p>
+			<p>Les données ne s’en sont pas remises et c’est avec grand regret que nous avons dû abandonner l’idée de récupérer la dernière version de la base pour récupérer celle de la nuit dernière. En conséquence, c’est tout une journée de jeu qui se retrouve perdue, entrainant avec elle les transactions effectuées en boutique, les inscriptions et les récupérations.  Nous allons bien entendu dans les heures qui suivent redistribuer tous les points acquis afin que vous puissiez refaire vos transactions.</p>
+			<p>La forge des âmes, ne semblant pas le moins du monde liée au crash, a subi elle aussi ce bond dans le temps et sera rétablie au plus vite.</p>
+			<p>Beaucoup de travail reste à faire pour tout remettre d’aplomb, nous vous tiendrons informés demain matin et tenterons de faire au mieux pour vous éviter plus de désagréments.</p>
+</div>
                     </div>
                 </div>
             <div class="encadrepage-bas">
