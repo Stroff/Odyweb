0b097e7e08a1378457289e4206a81cfccbf14569
niliah <niliah@odyssee-serveur.com>
2010-12-07 13:44:18 +0100

    message maintenance

--- maintenance.php
+++ maintenance.php
@@ -21,11 +21,16 @@ include "include/template/header_cadres.php"; ?>
 <h2>Maintenance</h2>
 <p style="padding-left:10px;padding-top:20px;">Le serveur est actuellement en maintenance. Veuillez revenir plus tard.</p>
 <br/> <br/> <br/> 						
-<p>Comme vous avez peut-être pu le constater par vous-même, le serveur a arrêté de fonctionner à 20h, n’ayant pu redémarrer correctement suite à un important crash de la base de données.</p>
-<p><b>Le serveur wow est donc redémarré, cependant l'accès au site et aux comptes est temporairement désactivé.</b></p>
+<p>Comme vous avez peut-être pu le constater par vous-même, le serveur a arrêté de fonctionner à 20h les 05 decembre, n’ayant pu redémarrer correctement suite à un important crash de la base de données.</p>
+<p><b>Le serveur wow a donc été redémarré, cependant l'accès au site et aux comptes est temporairement désactivé. </b></p>
 			<p>Les données ne s’en sont pas remises et c’est avec grand regret que nous avons dû abandonner l’idée de récupérer la dernière version de la base pour récupérer celle de la nuit dernière. En conséquence, c’est tout une journée de jeu qui se retrouve perdue, entrainant avec elle les transactions effectuées en boutique, les inscriptions et les récupérations.  Nous allons bien entendu dans les heures qui suivent redistribuer tous les points acquis afin que vous puissiez refaire vos transactions.</p>
-			<p>La forge des âmes, ne semblant pas le moins du monde liée au crash, a subi elle aussi ce bond dans le temps et sera rétablie au plus vite.</p>
-			<p>Beaucoup de travail reste à faire pour tout remettre d’aplomb, nous vous tiendrons informés demain matin et tenterons de faire au mieux pour vous éviter plus de désagréments.</p>
+			<p>La forge des âmes, ne semblant pas le moins du monde liée au crash, a été retablie.</p>
+
+			<p>Les comptes ayant effectué des achats le 5 decembre seront recredités, certainement au plus tard demain.</p>
+                        <p>Pour consoler les gens ayant perdu la journée de dimanche, nous avons decidé de mettre en place des rates x2 du samedi matin au dimanche soir.
+                            Ces rates seront repercutés sur l'xp, les loots, l'or, les points d'honneur, les metiers et réputations mais pas sur les emblèmes ou les points d'arene, ce paramètre n'étant pas modifiable. </p>
+                        <p> Pour les transactions et points de votes ayant disparu, nous avons décidé d'offrir 3 pody à tous les comptes odyssée. Vous recevrez également bientôt un cadeau de noël.</p>
+Nous esperons que ces mesures vous contenteront et dementreront notre souci constant de vous satisfaire, et vous souhaitons un bon week-end!<br/><br/><br/><br/>
 </div>
                     </div>
                 </div>
