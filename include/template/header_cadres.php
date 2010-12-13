64e6c1326dad5eed09204f38e973393095265407
fabed <fabed-webmaster@hotmail.fr>
2010-12-13 19:35:22 +0100

    Ajout du fetch row

--- include/template/header_cadres.php
+++ include/template/header_cadres.php
@@ -11,10 +11,9 @@ if(isset($_SESSION['id']))
 
 //Recherche compte dans account_banned
 	$search_acc_bann = mysql_query("SELECT COUNT(id) FROM account_banned WHERE id=".$_SESSION['id']);
-	$num_search_acc_bann = mysql_fetch_row($search_acc_bann);
 
 //Si le nombre de compte banni est different de 0 alors il est banni donc redirigé vers msg.php avec le msg numéro 1
-	if($num_search_acc_bann != 0)
+	if($search_acc_bann != 0)
 	{
 		$_SESSION['msg'] == "valide";
 		header("location: message/?msg=1");
