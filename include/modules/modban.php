f1d0e5860712e3ef887c560f81d1c97c4c1be64d
stroff <stroff@odyssee-serveur.com>
2010-12-15 12:57:48 +0100

    Ajout Ban IP

--- include/modules/modban.php
+++ include/modules/modban.php
@@ -1,4 +1,6 @@
 <?php
+	include "include/php/fonctions.php";
+
 	if(isset($_SESSION['id']))
 	{
 		//Connexion Base de donnée Reamld
@@ -18,5 +20,18 @@
 				. date("d/m/Y à H:m",$recher_fetch["unbandate"]).'</strong> !<br/>Raison : <strong>'
 				. $recher_fetch["banreason"].'</strong></center>';
 		}
+	}	
+	
+	//Recherche compte dans account_banned
+	$search_ip_bann = mysql_query("SELECT * FROM realmd.ip_banned WHERE ip=".get_ip().' AND FROM_UNIXTIME(unbandate) > NOW()');
+
+	//Si le nombre de compte banni est different de 0 alors il est banni donc redirigé vers msg.php avec le msg numéro 1
+	if(mysql_num_rows($search_ip_bann) != 0)
+	{
+		$recher_fetch = mysql_fetch_array($search_ip_bann);
+		echo '<center>'.$recher_fetch["bannedby"].' a banni votre IP depuis le <strong>'
+			. date("d/m/Y à H:m",$recher_fetch["bandate"]).'</strong> jusqu\'au <strong>'
+			. date("d/m/Y à H:m",$recher_fetch["unbandate"]).'</strong> !<br/>Raison : <strong>'
+			. $recher_fetch["banreason"].'</strong></center>';
 	}	
 ?>
