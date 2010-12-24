84592126e7862a12ca2bdea57de88d4a6378ddde
niliah <niliah@odyssee-serveur.com>
2010-12-24 11:47:56 +0100

    suppression du gm: cadeaux actifs

--- include/modules/modnoel.php
+++ include/modules/modnoel.php
@@ -1,7 +1,6 @@
-<?php if($_SESSION['gm']>2){ ?>
+
 <a href="cadeau_noel.php" title="Avez-vous été sage?"><img src="medias/images/noel.jpg"/> </a>
 
    <div class="spacer">
         </div>
 
-<?php } ?>
