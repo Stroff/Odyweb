e25ff6e361dcc45df886b95ecdb1c0f1f315cd74
Damien <damien@macuser.fr>
2010-12-27 13:28:57 +0100

    Fix d'un bug lors des ban des demandes de recups d'une personne.

--- recuperation.php
+++ recuperation.php
@@ -26,7 +26,7 @@
                             <div class="formulaires">
 								<?php
 								//check si le compte est bloqué au niveau des recups
-								$check_blocage_compte=mysql_query("SELECT fin_blocage FROM accounts_blocage_recup WHERE id_compte='".$compte_id."' ORDER BY fin_blocage DESC LIMIT 1");
+								$check_blocage_compte=mysql_query("SELECT fin_blocage FROM accounts_blocage_recup WHERE id_compte='".$compte_id."' AND fin_blocage > NOW() ORDER BY fin_blocage DESC LIMIT 1");
 								if(mysql_num_rows($check_blocage_compte)>0) {
 									$date_fin_blocage_compte = mysql_fetch_array($check_blocage_compte);
 									echo '<p>Votre compte est bloqué a cause d\'un trop grande nombre de demandes de récupérations refusée. Il serra débloqué automatiquement le '.$date_fin_blocage_compte["fin_blocage"].'</p>';
