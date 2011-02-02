0667f908450e545b0b44fb32638fb8d65b195c54
niliah <niliah@odyssee-serveur.com>
2011-02-02 11:26:43 +0100

    modifs faq install et ajout de "partenaires"

--- partenaires.php
+++ partenaires.php
@@ -1,3 +1,17 @@
+<?php require "include/template/header_cadres.php"  ?>
+
+        	<div class="encadrepage-haut">
+            	<div class="encadrepage-titre">
+                        <br/>
+                        <br/>
+           	            <img src="medias/images/titre/serveur.gif" >                    
+            	</div>
+            </div>
+            	<div class="blocpage">
+                	<div class="blocpage-haut">
+                    </div>
+                    <div class="blocpage-bas">
+                    	<div class="blocpage-texte">
 <?php $connexion = mysql_connect($host_site, $user_site , $pass_site);
 mysql_select_db($forum_database ,$connexion);
 mysql_query("SET NAMES 'utf8'");
@@ -19,8 +33,17 @@ $parser->parse_smilies          = 1;
 
 $messagesParPage=1; //Nous allons afficher 1 message par page.
 //Une connexion SQL doit être ouverte avant cette ligne...
-$post=mysql_query("SELECT * FROM forum.ibf_posts WHERE `pid` LIKE '193749';"); //Nous récupérons le contenu de la requête dans $post
+$post=mysql_query("SELECT * FROM forum.ibf_posts WHERE `pid` LIKE '75140';"); //Nous récupérons le contenu de la requête dans $post
 $donnees=mysql_fetch_array($post);
 	$post = $parser->preDisplayParse($donnees['post'] );
 	$post = str_replace("style_emoticons/<#EMO_DIR#>", "style_emoticons/default", $post);
 	echo $post;?>
+						
+                        	<br/> 	<br/> 	<br/> 
+					   </div>
+                    </div>
+             </div>
+            <div class="encadrepage-bas">
+            </div> 
+            
+<?php require "include/template/footer_cadres.php"?>
