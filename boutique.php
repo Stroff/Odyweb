69f9ff75e7d067424ca4254a5abd8fff02de99d0
Stroff <stroff@odyssee-serveur.com>
2011-03-17 18:24:48 +0100

    Desactivation des élèments de la boutique que la nouvelle version du core ne gère plus (à corriger).

--- boutique.php
+++ boutique.php
@@ -1,3 +1,6 @@
+<?php
+require_once 'secure.php';
+?>
 <?php 
 $secure_lvl = 1;
 $header_titre = "Liste de nos boutiques";
@@ -28,6 +31,8 @@ require "include/template/header_cadres.php";
 							<td><a href="boutique_objets.php?section=Armures"><img style = "border: 0px none ;" src="medias/images/boutique/armor.jpg" /></a></td>
 							<td><a href="boutique_objets.php?section=Compagnons"><img style = "border: 0px none ;" src="medias/images/boutique/compagnons.jpg" /></a></td>
 							<td><a href="boutique_objets.php?section=Montures"><img style = "border: 0px none ;" src="medias/images/boutique/mont.jpg" /></a></td> </tr>
+<?php
+if($_SESSION['gm'] >= 5){ ?>                            
 						<tr>
 							<td><a href="boutique_skin.php"><img style = "border: 0px none ;" src="medias/images/boutique/skin.jpg" /></a></td>
 							<td><a href="boutique_rename.php"><img style = "border: 0px none ;margin-top:15px;" src="medias/images/boutique/rename.jpg" /></a></td>	
@@ -37,7 +42,7 @@ require "include/template/header_cadres.php";
 							<td><a href="boutique_objets.php?section=Fun"><img style = "border: 0px none ;" src="medias/images/boutique/fun.jpg" /></a></td>
                                                         <td><a href="boutique_level_metiers.php"><img style = "border: 0px none ;" src="medias/images/boutique/metiers.jpg" /></a></td>
 						</tr>
-
+<?php } ?>
 						</table>
       	  <p align="center"><a href="account_boutique_logs.php" style = "">Historique de vos achats</a></p>
 
