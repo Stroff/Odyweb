a28e68fdae289e0daf28f28b60e8b30032516115
Stroff <stroff@odyssee-serveur.com>
2011-04-04 19:07:02 +0200

    Fixe Timer Joug

--- include/modules/modserver.php
+++ include/modules/modserver.php
@@ -20,15 +20,19 @@
 				$joueurs_online_alliance = $joueurs_online_alliance[0];
 
 				// connexion pour faire les mysql_escape_string
+				$joug_txt = "";
 				$connexion = mysql_connect($host_wow, $user_wow , $pass_wow);
 				mysql_select_db($wow_world ,$connexion);
 				mysql_query("SET NAMES 'utf8'");
-				$resultat_joug  = mysql_query("SELECT * FROM wintergrasp_save");
-				$joug_result = mysql_fetch_array($resultat_joug);
-				if($joug_result['war']==0){
-					$joug_txt= timestamp_to_h_m_s_relative($joug_result['timer']);
-				} else {
-					$joug_txt = "Joug en cours, encore ".timestamp_to_h_m_s_relative($joug_result['timer']);
+				$resultat_joug  = mysql_query("SELECT * FROM worldstates where entry in (3781, 3801)");
+				while($joug_result = mysql_fetch_array($resultat_joug)) {
+					if($joug_result['entry']==3801){
+						if($joug_result['value'] == 1)
+							$joug_txt .= "Joug en cours, encore ";
+					}
+					if($joug_result['entry']==3781){
+						$joug_txt .= timestamp_to_h_m_s_relative($joug_result['value']);
+					}
 				}
 
 
