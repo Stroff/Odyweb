5056d6065fd2245c844813480f455e6bc347c0ea
niliah <niliah@odyssee-serveur.com>
2011-04-08 11:32:23 +0200

    fix de fix :o

--- statistiques_arene.php
+++ statistiques_arene.php
@@ -10,7 +10,7 @@ Le top teams
 $connexion = mysql_connect($host_wow, $user_wow , $pass_wow);
 mysql_select_db($wow_characters ,$connexion);
 mysql_query("SET NAMES 'utf8'");
-$liste_arenes = mysql_query("select distinct c.name, t.name, s.rating, a.personal_rating, a.matchmaker_rating
+$liste_arenes = mysql_query("select distinct c.name, t.name, s.rating, a.personal_rating
 from characters as c, arena_team as t, arena_team_stats as s, arena_team_member as m, character_arena_stats as a
 where c.guid = m.guid and
 t.arenateamid=m.arenateamid and
@@ -26,7 +26,7 @@ echo '
                 <th>Nom</th>
 		<th>Team</th>
 		<th>Côte</th>
-		<th>Côte MMR de la team</th>
+		<th>Côte MMR personnelle</th>
 		</tr>
 </thead>
 	<tbody>';
