d4838ed87470cc048ee9e142a3fb4ed2b79e50ba
niliah <niliah@odyssee-serveur.com>
2011-05-13 17:02:26 +0200

    gmlogs

--- admin/gm_report.php
+++ admin/gm_report.php
@@ -33,8 +33,8 @@ if($_SESSION['gm'] < 5){
 <?php include "navbar.php"; include "config/config.php"; ?>
 <div id="dt_example">
 <?php 
-$date_debut = "2011-04-01";
-$date_fin = "2011-05-01";
+$date_debut = "2011-05-01";
+$date_fin = "2011-06-01";
 ?>
 
 <p>Rapport mensuel MJ</p>
