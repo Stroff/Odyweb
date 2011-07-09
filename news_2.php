9b23fc4d17f97758e380fe43f9c9729d3620d448
niliah <niliah@odyssee-serveur.com>
2011-07-09 15:27:19 +0200

    newsletter 2

---
+++ news_2.php
@@ -0,0 +1,73 @@
+<?php require "include/template/header_cadres.php" ?>
+
+<div class="encadrepage-haut">
+    <div class="encadrepage-titre">
+        <br/>
+        <br/>
+        <img src="medias/images/titre/serveur.gif" >
+    </div>
+</div>
+<div class="blocpage">
+    <div class="blocpage-haut">
+    </div>
+    <div class="blocpage-bas">
+        <div class="blocpage-texte">
+           <h1>Newsletter Odyssée – Il y a de la houle !</h1>
+
+
+Joueurs de divers horizons, passionnés de MMORPG, addicts de WoW, bonjour. <br/> <br/> 
+
+Odyssée, serveur blizzlike à votre service depuis plus de deux ans, vous invite à venir découvrir ses dernières nouveautés ! Ces dernières semaines, nous avons travaillé d'arrache-pied pour fournir à toutes et à tous la qualité de jeu qu'un serveur privé se doit d'offrir à ses joueurs.
+ Aussi était-il temps de faire un récapitulatif des nouveautés qui ont secoué notre communauté.<br/> <br/> 
+<h2>1/Sur la scène de l’émulation WOW</h2><br/> 
+<h3>Top votes : un debut de justice ?</h3><br/> 
+Vous vous en êtes peut-être rendus compte, l'effondrement de notre place sur RPG-P était dû aux bots votes mis en place par les serveurs privés eux-mêmes pour remonter aux classements. La différence avec un bot vote destiné à gagner des points boutique?
+<br/> 
+La où les bots votes pour Odyssée (largement combattus par notre équipe) ne touchent qu'à nos pody, les bots votes mis en places par les team eux-mêmes permettent aussi de contourner le captcha RPG-Paradize.
+Nous espérons que ce redressement continuera avec les autres sites coupables, et que cela n'est pas dû uniquement à la perte de la première place qu'avait perdue un certain serveur, ce qui aurait poussé la direction de RPG P à agir par amitié... Nous espérons que le leader en question, qui ne se prive pas de ces manœuvres non plus, subira en toute justice le même sort que ses collègues, comme ceux dont on trouve facilement les bots sur internet qui contournent le captcha RPG P.
+<br/> <br/> 
+Notre team a toujours souhaité rester honnête, pour nous aider, que faire?
+Renouveler d'attention et voter sur prg paradize pour que nous remontions à la place que nous méritons! En espérant que la solidarité et l’honnêteté dont nous avons fait preuve paiera enfin.
+<br/> <br/> 
+<h2>2/ Technique :</h2><br/> 
+<h3>Nouvelle saison </h3><br/> 
+Avec l’ouverture d’ICC et la mise à disposition du couroucé, voici une page de tournée pour repartir de plus belle ! <br/> <br/> 
+<h3>changement de core</h3><br/> 
+Notre core odyssée présentait tant de différences avec le core trinity d’origine qu’il devenait affreusement déséquilibré et difficile à mettre à jour, aussi sommes-nous repartis avec un core tout propre.
+Celui-ci est déjà bien plus performant et facile à corriger, nous avons déjà fait de nombreux patchs dessus (voir cette liste)<br/> <br/> 
+<h3>Priorité des debugs</h3><br/> 
+Maintenant, les joueurs eux-mêmes votent pour les debugs à passer en priorité. Votez grâce à cette page !<br/> <br/> 
+<h3>Soucis de comptes !</h3><br/> 
+Si vous avez le message « compte fermé » et qu’aucun message n’apparait sur l’accueil du site, contactez l’équipe, cela est dû à un bug auquel nous pouvons aisément remédier.
+<br/> <br/> 
+<h2>3/ Communauté</h2><br/> 
+<h3>Récupérations gratuites et mises au gout du jour</h3>
+Les recuperations standard sont dorenavant gratuites et disponibles en haineux/T7. Les premium elles offrent le T8/fatal.<br/> <br/> 
+<h3>Changements de factions</h3><br/> 
+Pour remédier au déséquilibre flagrant des factions, la team a tiré au sort 50 personnages désirant changer de race pour passer de la horde à l’alliance. 
+<br/> <br/> 
+<h3>Cadeau </h3><br/> 
+Pour vous remercier de votre fidélité, la team a offert 3 pody par compte.<br/> <br/> 
+<h3>Clôture de la saison de votes </h3>
+Bravo à petitbelin, pbe13 et nelloche qui ont gagné chacun 50, 40 et 20€ de chèque cadeau pixmania sans utiliser de bot vote !
+<br/> <br/> 
+<h3>Embauche </h3><br/> 
+Nous recherchons activement des testeurs, animateurs, développeurs SQL et C++… Si cela vous intéresse, petit message privé a Braise via le forum avec une courte présentation !
+
+<br/> <br/> <br/> 
+<h2>Pour conclure:</h2><br/> 
+Voilà un bref résumé des dernières news : vous pouvez consulter le détail de ces news sur notre site et notre forum, saurez-vous faire évoluer votre regard comme nous nous sommes battus pour regagner votre cœur ?
+
+<br/> <br/> 
+<strong>Rappelez-vous que pour le meilleur et pour le pire, notre motivation, c'est vous ! </strong>
+<br/> 
+Avec amour, le staff Odyssée
+
+            <br/> 	<br/> 	<br/>
+        </div>
+    </div>
+</div>
+<div class="encadrepage-bas">
+</div> 
+
+<?php require "include/template/footer_cadres.php" ?>
